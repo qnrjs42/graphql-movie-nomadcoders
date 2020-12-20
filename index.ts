ceca6651904ad8a273f85197ca2b75c0bd696076
qnrjs42 <qnrjs42@naver.com>
2020-12-20 23:19:44 +0900

    0-5. Creating the first Query and Resolver

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import { GraphQLServer } from 'graphql-yoga';
+import resolvers from './graphql/resolvers';
 
 const server = new GraphQLServer({
-
+  typeDefs: "graphql/schema.graphql",
+  resolvers: resolvers
 });
 
+// localhost:4000
 server.start(() => console.log("Graphql Server Running"));
