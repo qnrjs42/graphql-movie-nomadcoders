3469d28b4e16cbba5aede48884b45b55ac4c7521
qnrjs42 <qnrjs42@naver.com>
2020-12-21 21:16:38 +0900

    0-12. Wrapping a REST API with GraphQL Part One

--- graphql/db.ts
+++ graphql/db.ts
@@ -1,45 +1,56 @@
-export let movies = [
-  {
-    id: 1,
-    name: "Star Wars",
-    score: 1
-  },
-  {
-    id: 2,
-    name: "Avengers",
-    score: 99
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 100
-  }
-]
+import fetch from 'node-fetch';
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.am/api/v2/list_movies.json";
 
-export const getById = (id: number) => {
-  const filteredMovies = movies.filter(person => person.id === id);
-  return filteredMovies[0];
+export const getMovies = (limit, rating) => {
+  fetch(`${API_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 }
 
-export const deleteMovie = (id: number) => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if(movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-}
 
-export const addMovie = (name: string, score: number) => {
-  const newMovie = {
-    id: movies.length + 1,
-    name,
-    score
-  }
-  movies.push(newMovie);
-  return newMovie
-}
+// export let movies = [
+//   {
+//     id: 1,
+//     name: "Star Wars",
+//     score: 1
+//   },
+//   {
+//     id: 2,
+//     name: "Avengers",
+//     score: 99
+//   },
+//   {
+//     id: 3,
+//     name: "Logan",
+//     score: 100
+//   }
+// ]
+
+// export const getMovies = () => movies;
+
+// export const getById = (id: number) => {
+//   const filteredMovies = movies.filter(person => person.id === id);
+//   return filteredMovies[0];
+// }
+
+// export const deleteMovie = (id: number) => {
+//   const cleanedMovies = movies.filter(movie => movie.id !== id);
+//   if(movies.length > cleanedMovies.length) {
+//     movies = cleanedMovies;
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// export const addMovie = (name: string, score: number) => {
+//   const newMovie = {
+//     id: movies.length + 1,
+//     name,
+//     score
+//   }
+//   movies.push(newMovie);
+//   return newMovie
+// }
 
--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,15 +1,19 @@
-import { getById, getMovies, addMovie, deleteMovie } from "./db";
+import { getMovies } from './db'; 
 
 
-const resolvers = {
-  Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id)
-  },
-  Mutation: {
-    addMovie: (_, {name, score}) => addMovie(name, score),
-    deleteMovie: (_, {id}) => deleteMovie(id)
-  }
-}
+
+// import { getById, getMovies, addMovie, deleteMovie } from "./db";
+
+
+// const resolvers = {
+//   Query: {
+//     movies: () => getMovies(),
+//     movie: (_, { id }) => getById(id)
+//   },
+//   Mutation: {
+//     addMovie: (_, {name, score}) => addMovie(name, score),
+//     deleteMovie: (_, {id}) => deleteMovie(id)
+//   }
+// }
 
 export default resolvers;
