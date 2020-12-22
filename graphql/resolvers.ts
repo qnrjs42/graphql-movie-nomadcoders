6eea075a84ed43da249493d856c64d2f4ea23525
qnrjs42 <qnrjs42@naver.com>
2020-12-22 20:44:38 +0900

    getMovies, getMovie, getMovieSuggestions

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,22 +1,11 @@
-import { getMovies } from './db'; 
-// import { getById, getMovies, addMovie, deleteMovie } from "./db";
+import { getMovies, getMovie, getSuggestions } from "./db";
 
 const resolvers = {
   Query: {
-    movies: (_, { rating, limit }) => getMovies(limit, rating)
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
   }
 };
-
-
-// const resolvers = {
-//   Query: {
-//     movies: () => getMovies(),
-//     movie: (_, { id }) => getById(id)
-//   },
-//   Mutation: {
-//     addMovie: (_, {name, score}) => addMovie(name, score),
-//     deleteMovie: (_, {id}) => deleteMovie(id)
-//   }
-// }
 
 export default resolvers;
